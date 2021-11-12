eba448d3c1eebb7f465be664e5be53e05abca707
hplisiecki <57173133+hplisiecki@users.noreply.github.com>
2021-11-12 18:26:25 +0100

    Update tfidf_saturation.py

--- implementation/tfidf_saturation.py
+++ implementation/tfidf_saturation.py
@@ -3,11 +3,10 @@ import pandas as pd
 from gensim.utils import simple_preprocess
 from gensim.models import TfidfModel
 from gensim import corpora
-import numpy as np
 from scipy import spatial
 from tqdm import tqdm
-
-
+import numpy as np
+import math
 
 
 class EmbeddingSimilarity(object):
@@ -16,29 +15,51 @@ class EmbeddingSimilarity(object):
 
     def __init__(self, corpus, size = 300, window = 5, min_count = 5, workers = 4, model = None):
         # preprocess
-        doc_tokenized = [simple_preprocess(doc) for doc in corpus]
-        if model is not None:
+        self.tokenized_documents = [simple_preprocess(doc) for doc in corpus]
+        if model is None:
             print('Training the word embeddings')
             # train the word embeddings
-            self.model = Word2Vec(sentences = doc_tokenized, vector_size=size, window=window, min_count=min_count, workers=workers)
+            self.model = Word2Vec(sentences = self.tokenized_documents, vector_size=size, window=window, min_count=min_count, workers=workers)
         print('Computing the TF-IDF scores')
         # self.scores = self.tfidf(corpus)
         self.dictionary = corpora.Dictionary()
         # create a bag of words for each document
-        self.bow = [self.dictionary.doc2bow(doc, allow_update=True) for doc in doc_tokenized]
+        self.bow = [self.dictionary.doc2bow(doc, allow_update=True) for doc in self.tokenized_documents]
         # create a tf-idf model
         self.tfidf_scores = TfidfModel(self.bow, smartirs='ntc')
         # creaste a dataframe out of the corpus
         print('Creating the Document Embeddings')
         self.df = pd.DataFrame(corpus, columns=['text'])
         # create the tf-idf weighted doc embeddings
-        self.df['embeddings'] = self.create_embedding(self.model, self.df['text'])
+        self.df['embeddings'] = self.create_embeddings(self.model, self.df['text'])
 
 
     def calculate_similarity(self, query):
         # calculate the cosine similarity between the embedding and all documents in the dataframe
+        if len(query.split()) > 1:
+            embeddings = []
+            retained = ''
+            # for each word in the query
+            for q in query.split():
+                try:
+                    # get the embedding of the word
+                    emb = self.model.wv[q]
+                    retained = retained + q + ' '
+                    # add the embedding weighted by the idf to the embedding set
+                    embeddings.append(emb * self.idf(q, self.tokenized_documents))
 
-        query_embedding = self.model.wv[query]
+                except:
+                    print(f"{q} is not in the vocabulary. It will be omitted.")
+            # average the embeddings
+            query_embedding = np.mean(embeddings, axis=0)
+            print(f'Performing saturation for the following query: {retained}')
+        else:
+            try:
+                query_embedding = self.model.wv[query]
+                print(f'Performing saturation for the following query: {query}')
+            except:
+                print('The query is not in the vocabulary.')
+                return
         # create a list of cosine similarities
         similarities = self.df['embeddings'].apply(lambda row: self.cosine_similarity(query_embedding, row))
         # return the cosine similarity
@@ -50,7 +71,7 @@ class EmbeddingSimilarity(object):
         return 1 - spatial.distance.cosine(embedding1, embedding2)
 
 
-    def create_embedding(self, model, corpus):
+    def create_embeddings(self, model, corpus):
         # create the embedding for each document
 
         embedding_set = []
@@ -72,11 +93,21 @@ class EmbeddingSimilarity(object):
                     continue
             # create an embedding by averaging the word embeddings
             embedding = np.mean(embeddings, axis=0)
+
             # add the embedding to the embedding set
             embedding_set.append(embedding)
         # return the embedding set
         return embedding_set
-    
+
+    def idf(self, word, tokenized_documents):
+        # calculate the inverse document frequency of a word
+        n = len(tokenized_documents)
+        df = 0
+        for doc in tokenized_documents:
+            if word in doc:
+                df += 1
+        return math.log(n / df)
+
 # load the after_averaging.csv file
 df = pd.read_csv('after_averaging.csv')
 
@@ -91,5 +122,3 @@ df.sort_values(by=['demokracja'], ascending=False, inplace=True)
 
 # print the top 10 documents
 print(df.head(10))
-
-
